0c15ac0936a45d943c13b9609b874634c2797abb
SujeetPa <41929384+SujeetPa@users.noreply.github.com>
2026-06-29 06:08:19 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "GitOps Demo App v1 - updated1.3"
+    return "GitOps Demo App v1 - updated1.4"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
